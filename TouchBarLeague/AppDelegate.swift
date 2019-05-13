5518e2143b7a52de0eb646960d2e4c85209348e3
Marcin Ślusarek <mrvrota@gmail.com>
2019-05-13 14:51:37 +0200

    Initial Commit

---
+++ TouchBarLeague/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TouchBarLeague
+//
+//  Created by Marcin Slusarek on 13/05/2019.
+//  Copyright © 2019 Marcin Slusarek. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TouchBarLeague/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TouchBarLeague
+//
+//  Created by Marcin Slusarek on 13/05/2019.
+//  Copyright © 2019 Marcin Slusarek. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
